9b77446956220df3f37a28d37b15a7930bc12019
Shreejit Pillai <sreejit.pillai23@gmail.con>
2020-06-08 20:00:49 +0530

    First commit

--- camera.py
+++ camera.py
@@ -7,10 +7,11 @@ frameRate = 20.0
 
 class VideoCamera(object):
     def __init__(self):
+        print("Initialized")
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
-        self.video = cv2.VideoCapture("/dev/video1") #"/dev/video1"
+        self.video = cv2.VideoCapture(0) #"/dev/video1"
         self.video.set(3, 400)
         self.video.set(4, 560)
         # If you decide to use video.mp4, you must have this file in the folder
@@ -19,56 +20,20 @@ class VideoCamera(object):
     
     def __del__(self):
         self.video.release()
-    
+    #
     def get_frame(self):
 
         img_counter = 0
         success, image = self.video.read()
+        #image = cv2.imread("images/download.jpeg")
+        # image = cv2.resize(img, (560,400), interpolation=cv2.INTER_AREA)
+
         im = cv2.imread("newmask.png")
 
         # cv2.rectangle(image, (400, 300), (700, 500), (178, 190, 181), 5)
 
         frame = cv2.flip(image, 2)
-        # print("Frame size :", frame.shape[1])
 
-        # gaze.refresh(frame)
-        #
-        # frame, x, y = gaze.annotated_frame()
-        # text = ""
-        #
-        # left_pupil = gaze.pupil_left_coords()
-        #
-        # right_pupil = gaze.pupil_right_coords()
-        # # print(right_pupil, left_pupil)
-        #
-        # points_cnt = (x, y)
-        #
-        # cv2.putText(frame, " click any key  ", (175, 17), cv2.FONT_HERSHEY_DUPLEX, 0.7,
-        #             (0, 0, 0), 2)
-        # if left_pupil and right_pupil != None:
-        #     a = left_pupil
-        #     b = right_pupil
-        #     c = points_cnt
-        #
-        #
-        #     dst_left = distance.euclidean(a, c)
-        #     mm = 0.26458333
-        #     dist_left_mm = (dst_left * mm) + 8
-        #
-        #     # print(dist_left_mm)
-        #     # print("left:::", dist_left_mm)
-        #     dst_right = distance.euclidean(b, c)
-        #
-        #     dist_right_mm = (dst_right * mm) + 8
-        #     total_pd = dist_right_mm + dist_left_mm
-        #     # print("total::", total_pd)
-        #     # print("right::", dist_right_mm)
-        #
-        #     cv2.putText(frame, "Left PD:  " + str(dist_left_mm) + 'mm', (85, 125), cv2.FONT_HERSHEY_DUPLEX, 0.9,
-        #                 (0, 0, 255), 1)
-        #     cv2.putText(frame, "Right PD: " + str(dist_right_mm) + 'mm', (85, 175), cv2.FONT_HERSHEY_DUPLEX, 0.9,
-        #                 (0, 0, 255), 1)
-        #     k = cv2.waitKey(1)
         gaze.refresh(frame)
 
         frame, x, y = gaze.annotated_frame()
